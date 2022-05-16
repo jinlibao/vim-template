20956345ef4c50cd4856ecc8e8ecf6ee864215ea
Libao Jin <jinlibao@outlook.com>
2022-05-16 16:37:10 -0600

    Update C++ templates

--- templates/=template=c.cc
+++ templates/=template=c.cc
@@ -1,6 +1,6 @@
 /**
  * %FFILE%
- * Author: Libao Jin
+ * Author: %USER%
  * Date: %FDATE%
  */
 
@@ -10,25 +10,13 @@ using namespace std;
 
 #define debug(x) cerr << #x << " = " << x << '\n';
 
-#define rep(i, begin, end)                   \
-  for (auto i = (begin) - ((begin) > (end)); \
-       i != (end) - ((begin) > (end));       \
-       i += 1 - 2 * ((begin) > (end)))
-
-static const auto _ = [](){
+static const auto _ = []() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.precision(10);
   cout << fixed;
   return 0;
 }();
-
-#ifdef LOCAL
-ifstream fin("%FILE%.in");
-ofstream fout("%FILE%.out");
-#define cin fin
-#define cout fout
-#endif
 
 void solve() {
   %HERE%
@@ -39,7 +27,6 @@ int main(int argc, char* argv[]) {
   int T;
   cin >> T;
   for (int t = 1; t <= T; ++t) {
-    cout << "Case #" << t << ": ";
     solve();
   }
   return 0;
--- templates/=template=sol.cc
+++ templates/=template=sol.cc
@@ -1,6 +1,6 @@
 /**
  * %FFILE%
- * Author: Libao Jin
+ * Author: %USER%
  * Date: %FDATE%
  */
 
@@ -8,12 +8,26 @@
 
 using namespace std;
 
+#define debug(x) cerr << #x << " = " << x << '\n';
+
+static const auto _ = []() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.precision(10);
+  cout << fixed;
+  return 0;
+}();
+
 void solve() {
   %HERE%
   cout << '\n';
 }
 
 int main(int argc, char* argv[]) {
-  solve();
+  int T;
+  cin >> T;
+  for (int t = 1; t <= T; ++t) {
+    solve();
+  }
   return 0;
 }
